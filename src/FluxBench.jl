e9a167d24e3a37611caa907658e01c6d68de94e1
Sören Dobberschütz <sdobberschuetz@gmx.net>
2021-10-16 17:16:19 +0200

    Tests for FluxArchitectures only

--- src/FluxBench.jl
+++ src/FluxBench.jl
@@ -13,7 +13,7 @@ using Statistics
 using Zygote
 # using Torch - If we want to compare progress
 
-const MODELS = (ResNet, DenseNet, GoogleNet, VGG19) #, SqueezeNet)
+const MODELS = (ResNet, DenseNet, GoogleNet, VGG19) # , SqueezeNet)
 
 SUITE = BenchmarkGroup()
 
@@ -26,22 +26,22 @@ include("packages/fluxarchitectures.jl")
 include("bench.jl")
 
 
-function submit(submit = false, SUITE = SUITE)
-  bench()
-  warmup(SUITE, verbose = true)
-  results = run(SUITE, verbose = true)
-  println(results)
+function submit(submit=false, SUITE=SUITE)
+    bench()
+    warmup(SUITE, verbose=true)
+    results = run(SUITE, verbose=true)
+    println(results)
   
-  flat_results = flatten(results)
+    flat_results = flatten(results)
 
-  print(JSON.json(flat_results))
-  println()
+  # print(JSON.json(flat_results))
+  # println()
 
-  if submit
-    HTTP.post("$(ENV["CODESPEED_SERVER"])/result/add/json/",
+    if submit
+        HTTP.post("$(ENV["CODESPEED_SERVER"])/result/add/json/",
               ["Content-Type" => "application/x-www-form-urlencoded"],
               HTTP.URIs.escapeuri(Dict("json" => JSON.json(flat_results))))
-  end
+    end
 end
 
 end # module
