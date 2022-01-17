45cba445a35a22344120aea240b914d336a158ce
Sören Dobberschütz <sdobberschuetz@gmx.net>
2022-01-17 18:37:49 +0100

    Deactivate non FA-benchmarks

--- src/bench.jl
+++ src/bench.jl
@@ -1,35 +1,35 @@
 # CUDA.device!(3)
 
-const METALHEAD_MODELS = ((ResNet18, 18), (ResNet34, 34), (ResNet50, 50),
-                          (() -> DenseNet((6, 12, 24, 16)), 121),
-                          (GoogLeNet, nothing),
-                          (VGG19, 19)) #, SqueezeNet)
+# const METALHEAD_MODELS = ((ResNet18, 18), (ResNet34, 34), (ResNet50, 50),
+#   (() -> DenseNet((6, 12, 24, 16)), 121),
+#   (GoogLeNet, nothing),
+#   (VGG19, 19)) #, SqueezeNet)
 
-function benchmark_cu(model, batchsize = 64, config = nothing, group = nothing)
-  resnet = model
-  ip = rand(Float32, 224, 224, 3, batchsize)
+# function benchmark_cu(model, batchsize = 64, config = nothing, group = nothing)
+#   resnet = model
+#   ip = rand(Float32, 224, 224, 3, batchsize)
 
-  group["Forward_Pass_$(typeof(model))_$(config)_with_batchsize_$(batchsize)"] = b = @benchmarkable(
-        fw(gresnet, gip),
-        setup = (gresnet = $resnet |> gpu;
-               gip = gpu($ip)),
-        teardown = (GC.gc(); CUDA.reclaim()))
-end
+#   group["Forward_Pass_$(typeof(model))_$(config)_with_batchsize_$(batchsize)"] = b = @benchmarkable(
+#     fw(gresnet, gip),
+#     setup = (gresnet = $resnet |> gpu;
+#     gip = gpu($ip)),
+#     teardown = (GC.gc(); CUDA.reclaim()))
+# end
 
-function benchmark_bw_cu(model, batchsize = 64, config = nothing, group = nothing)
-  resnet = model
-  ip = rand(Float32, 224, 224, 3, batchsize)
+# function benchmark_bw_cu(model, batchsize = 64, config = nothing, group = nothing)
+#   resnet = model
+#   ip = rand(Float32, 224, 224, 3, batchsize)
 
-  group["Backwards_Pass_$(typeof(model))_$(config)_with_batchsize_$(batchsize)"] = b = @benchmarkable(
-        bw(gresnet, gip),
-        setup = (gresnet = $resnet |> gpu;
-   	       gip = gpu($ip)),
-        teardown = (GC.gc(); CUDA.reclaim()))
+#   group["Backwards_Pass_$(typeof(model))_$(config)_with_batchsize_$(batchsize)"] = b = @benchmarkable(
+#     bw(gresnet, gip),
+#     setup = (gresnet = $resnet |> gpu;
+#     gip = gpu($ip)),
+#     teardown = (GC.gc(); CUDA.reclaim()))
 
-end
+# end
 
 function bench()
-  mhead_group = addgroup!(SUITE, "Metalhead")
+  # mhead_group = addgroup!(SUITE, "Metalhead")
   # for model in METALHEAD_MODELS, n in (5, 10)
   #   # we can go higher with the batchsize
   #   # but the CI machines would have variable VRAM
@@ -41,10 +41,10 @@ function bench()
   # end
 
   # ObjectDetector
-  od_group = addgroup!(SUITE, "ObjectDetector")
-  for model in [ObjectDetector.YOLO.v3_608_COCO, ObjectDetector.YOLO.v3_tiny_416_COCO], batchsize in [1, 3]
-    objectdetector_add_yolo_fw(model, batchsize, od_group)
-  end
+  # od_group = addgroup!(SUITE, "ObjectDetector")
+  # for model in [ObjectDetector.YOLO.v3_608_COCO, ObjectDetector.YOLO.v3_tiny_416_COCO], batchsize in [1, 3]
+  #   objectdetector_add_yolo_fw(model, batchsize, od_group)
+  # end
 
   # # DiffEqFlux
   # ## NeuralODE
@@ -70,19 +70,19 @@ function bench()
 
 
   # Flux3D.jl
-  flux3d_group = addgroup!(SUITE, "Flux3D")
-  flux3d_add_trimesh(flux3d_group)
+  # flux3d_group = addgroup!(SUITE, "Flux3D")
+  # flux3d_add_trimesh(flux3d_group)
 
 
   # FluxArchitectures
-  fa_gpu_group = addgroup!(SUITE, "FluxArchitectures_GPU")
-  fluxarchitectures_add_darnn(FA_GPU(), 5, 5, 10, 10, 300, fa_gpu_group)
-  fluxarchitectures_add_dsanet(FA_GPU(), 3, 3, 4, 1, 3, 2, 10, 50, 1000, fa_gpu_group)
-  fluxarchitectures_add_lstnet(FA_GPU(), 2, 3, 10, 60, 20, 500, fa_gpu_group)
-  fluxarchitectures_add_tpalstm(FA_GPU(), 10, 10, 10, 300, fa_gpu_group)
-  # fa_cpu_group = addgroup!(SUITE, "FluxArchitectures_CPU")
-  # fluxarchitectures_add_darnn(FA_CPU(), 5, 5, 10, 10, 300, fa_cpu_group)
-  # fluxarchitectures_add_dsanet(FA_CPU(), 3, 3, 4, 1, 3, 2, 10, 50, 1000, fa_cpu_group)
-  # fluxarchitectures_add_lstnet(FA_CPU(), 2, 3, 10, 60, 20, 500, fa_cpu_group)
-  # fluxarchitectures_add_tpalstm(FA_CPU(), 10, 10, 10, 300, fa_cpu_group)
+  # fa_gpu_group = addgroup!(SUITE, "FluxArchitectures_GPU")
+  # fluxarchitectures_add_darnn(FA_GPU(), 5, 5, 10, 10, 300, fa_gpu_group)
+  # fluxarchitectures_add_dsanet(FA_GPU(), 3, 3, 4, 1, 3, 2, 10, 50, 1000, fa_gpu_group)
+  # fluxarchitectures_add_lstnet(FA_GPU(), 2, 3, 10, 60, 20, 500, fa_gpu_group)
+  # fluxarchitectures_add_tpalstm(FA_GPU(), 10, 10, 10, 300, fa_gpu_group)
+  fa_cpu_group = addgroup!(SUITE, "FluxArchitectures_CPU")
+  fluxarchitectures_add_darnn(FA_CPU(), 5, 5, 10, 10, 300, fa_cpu_group)
+  fluxarchitectures_add_dsanet(FA_CPU(), 3, 3, 4, 1, 3, 2, 10, 50, 1000, fa_cpu_group)
+  fluxarchitectures_add_lstnet(FA_CPU(), 2, 3, 10, 60, 20, 500, fa_cpu_group)
+  fluxarchitectures_add_tpalstm(FA_CPU(), 10, 10, 10, 300, fa_cpu_group)
 end
